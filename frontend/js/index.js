c2ca76d967f17f544e144fcdba4421319e1ffa11
Audace Kwizera <companyfreshbeauty@gmail.com>
2021-07-29 19:40:42 +0200

    Test 4 - Affichage des articles dans le console log des cloneElt => ok / pas d'affichage visuel => ko

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -9,19 +9,21 @@ async function main() {
     //Attendre que les produits soit trouvés par fetch pour les récuperer des produits
     const products = await getProducts()
     //console.log(products)
+    //Selection de chaque produits de la liste de produits
+    for (product of products)
     //Fonctions qui affiche tous les produits
-    displayProducts(products)
+    displayProduct(product)
 }
 
 function getProducts() {
     //Chercher et récupérer les données via fetch
     return fetch("http://localhost:3000/api/teddies")
         //Transformer données en json
-        .then(function (httpBodyResponse) {
+        .then(function(httpBodyResponse) {
             return httpBodyResponse.json()
         })
         //Récuperation de tous les produits (json)
-        .then(function (products) {
+        .then(function(products) {
             return products
             /**
              * Test 1 => récupération des données ok le fetch récupère les donnés
@@ -38,7 +40,16 @@ function getProducts() {
         })
 }
 
+//Affichage des produits
+function displayProduct(product) {
+    //Copie et ajout du template html
+    const templateElt = document.getElementById("templateProduct")
+    const cloneElt = document.importNode(templateElt.content, true)
 
-function displayProducts() {
-
+    //Remplir les infos du template par les infos de l'api
+    cloneElt.getElementById("product__image").src = product.imageUrl
+    cloneElt.getElementById("product__title").textContent = product.name
+    cloneElt.getElementById("product__description").textContent = product.description
+    cloneElt.getElementById("product__price").textContent = product.price
+    //console.log(cloneElt)
 }
