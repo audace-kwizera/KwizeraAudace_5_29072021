669b1cb2640fe0fd1da0390a61e561e7f3c90d7d
Audace Kwizera <companyfreshbeauty@gmail.com>
2021-07-29 17:55:18 +0200

    code base crée

---
+++ frontend/js/index.js
@@ -0,0 +1,22 @@
+const { default: PagesManifestPlugin } = require("next/dist/build/webpack/plugins/pages-manifest-plugin");
+
+/**
+ * Fonction main qui va contenir le code de base pour éviter
+ * les variables au niveau du script de base
+ */
+main()
+
+function main() {
+    //Recupération des produits
+    const products = getProducts();
+    //Fonctions qui affiche tous les articles
+    displayProducts(products);
+}
+
+function getProducts() {
+    
+}
+
+function displayProducts() {
+
+}
