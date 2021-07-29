649daf61550d71810b4eb691f5c8d7483e01d40f
Audace Kwizera <companyfreshbeauty@gmail.com>
2021-07-29 18:09:31 +0200

    Test 1 : Le fichier index est bien connecté à l'api qui fonctionne bien et le fetch pour récuperer les données fonctionne correctement

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,3 @@
-const { default: PagesManifestPlugin } = require("next/dist/build/webpack/plugins/pages-manifest-plugin");
-
 /**
  * Fonction main qui va contenir le code de base pour éviter
  * les variables au niveau du script de base
@@ -8,13 +6,25 @@ main()
 
 function main() {
     //Recupération des produits
-    const products = getProducts();
-    //Fonctions qui affiche tous les articles
-    displayProducts(products);
+    const products = getProducts()
+    //Fonctions qui affiche tous les produits
+    displayProducts(products)
 }
 
 function getProducts() {
-    
+    //Chercher et récupérer les données via fetch
+    fetch("http://localhost:3000/api/teddies")
+        //Transformer données en json
+        .then(function (httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        //Récuperation de tous les produits (json)
+        .then(function (produits) {
+            /**
+             * Test de récupération le fetch récupère les donnés
+             * console.log(produits)
+             */
+        })
 }
 
 function displayProducts() {
