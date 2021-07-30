53bf2c8c22684402b1effebd9715fd273b6c8702
Audace Kwizera <companyfreshbeauty@gmail.com>
2021-07-30 22:26:50 +0200

    Test 8 Récuperation d'un seul article sur la page produit => ok

--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -4,9 +4,10 @@
 //Création d'une fontion no name appelé grâce à () pour récupèrer l'id de notre url
 (async function() {
     const productId = getProductId()
-    console.log(productId)
     //Appel du produit
-    const product = getProduct(productId)
+    //Attendre que les produits soit trouvés par fetch pour les récuperer des produits
+    const product = await getProduct(productId)
+    console.log(product)
     //Changer le contenu de notre div existante, on rafraichi en récupérant les infos sur notre produit
     hydrateProduct(product)
 })()
@@ -17,7 +18,28 @@ function getProductId() {
 }
 
 function getProduct(productId) {
-
+    //Chercher et récupérer les données via fetch
+    return fetch(`http://localhost:3000/api/teddies/${productId}`)
+        //Transformer données en json
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        //Récuperation de tous les produits (json)
+        .then(function(products) {
+            return products
+            /**
+             * Test 1 => récupération des données ok le fetch récupère les donnés
+             * console.log(products)
+             */
+        })
+        //Affichage d'un message au cas ou il y a une erreur 
+        .catch(function(error) {
+            alert(error)
+            /**
+             * Test 2 => Affichage d'une erreur si aucun produit trouvé
+             * console.log(error)
+             */
+        })
 }
 
 function hydrateProduct(product) {
