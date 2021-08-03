be469c4af53ccc259f1a2b151eec212d4ddce745
Audace Kwizera <companyfreshbeauty@gmail.com>
2021-08-03 22:28:41 +0200

    ajout du template html du panier // // Calcul du total selon quantite => ko // forcer le formulaire quantite a rester positif => ko

---
+++ frontend/js/panier.js
@@ -0,0 +1,5 @@
+//Recuperer les produit dans la page panier
+//Verifier si le local storage est vide, on va lire la clé produit via getItem
+let localStorageInit = JSON.parse(localStorage.getItem("product"))
+//conversion des données js dans le localstorage en json via json.parse
+console.log(localStorageInit)
